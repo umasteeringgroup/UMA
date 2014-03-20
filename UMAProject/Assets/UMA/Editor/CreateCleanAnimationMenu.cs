d24aed8faf4a7ec17417ff93cf17bae506f03b36
Joen-UnLogick <joengit@glscene.com>
2014-03-20 23:54:24 +0100

    Added MenuItem to clean animations for position and scale. Useful for animated additional bones.

---
+++ UMAProject/Assets/UMA/Editor/CreateCleanAnimationMenu.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class CreateCleanAnimationMenu : MonoBehaviour {
+
+	[MenuItem("UMA/Create Clean Animation")]
+	static void CreateCleanAniamtionMenuItem()
+	{
+		foreach(var obj in Selection.objects)
+		{
+			var anim = obj as AnimationClip;
+			if (anim != null)
+			{
+				//var newClip = new AnimationClip();
+				var newClip = Instantiate(anim) as AnimationClip;
+				newClip.ClearCurves();
+				AnimationUtility.SetAnimationType(newClip, ModelImporterAnimationType.Human);
+				var curves = AnimationUtility.GetAllCurves(anim);
+				foreach (var curve in curves)
+				{
+					if (!curve.propertyName.StartsWith("m_LocalScale") && !curve.propertyName.StartsWith("m_LocalPosition"))
+					{
+						newClip.SetCurve(curve.path, curve.type, curve.propertyName, curve.curve);
+					}
+				}
+
+
+				var oldPath = AssetDatabase.GetAssetPath(anim);
+				var folder = System.IO.Path.GetDirectoryName(oldPath);
+
+				AssetDatabase.CreateAsset(newClip, AssetDatabase.GenerateUniqueAssetPath( folder + "/" + anim.name + ".anim"));
+				AssetDatabase.SaveAssets();
+				//var sourceSO = new SerializedObject(anim);
+				//var so = new SerializedObject(newClip);
+				//var clipSettings = so.FindProperty("m_AnimationClipSettings");
+				//clipSettings.objectReferenceValue = sourceSO.FindProperty("m_AnimationClipSettings").objectReferenceValue;
+				//sourceSO.Dispose();
+				//so.Dispose();
+			}
+		}
+	}
+
+}
