df7b2f3eac1d5e6fe06ef0b0068c22f6358119e8
ecurtz <eli@nuprometheus.com>
2015-03-06 07:49:13 -0800

    Removed final reference to usePRO in texture generation.

--- UMAProject/Assets/UMA/Example/Scripts/UMAGenerator.cs
+++ UMAProject/Assets/UMA/Example/Scripts/UMAGenerator.cs
@@ -5,18 +5,6 @@ public class UMAGenerator : UMAGeneratorBuiltin
 {
 	public override void Awake()
 	{
-#if ! UNITY_5
-		if (usePRO)
-		{
-#if UNITY_EDITOR
-			if (!UnityEditorInternal.InternalEditorUtility.HasPro())
-			{
-				Debug.LogError("You told the Generator to usePRO but you don't have pro license. Toggling to false.", gameObject);
-				usePRO = false;
-			}
-#endif
-		}
-#endif
 		base.Awake();
 	}
 
