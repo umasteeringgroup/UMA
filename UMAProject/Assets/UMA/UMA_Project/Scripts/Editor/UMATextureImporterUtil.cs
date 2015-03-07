eb1066651cda62c3e7af3f6f4dd58f8b3dd3acbe
Joen-UnLogick <joengit@glscene.com>
2015-03-07 18:46:47 +0100

    Updated the UMATextureImporterUtil to trim the bones list.

--- UMAProject/Assets/UMA/UMA_Project/Scripts/Editor/UMATextureImporterUtil.cs
+++ UMAProject/Assets/UMA/UMA_Project/Scripts/Editor/UMATextureImporterUtil.cs
@@ -216,9 +216,21 @@ namespace UMAEditor
                 }
             }
 
-            Mesh resultingMesh = SeamRemoval.PerformSeamRemoval(resultingSkinnedMesh, prefabMesh, 0.0001f);
-            resultingSkinnedMesh.sharedMesh = resultingMesh;
-            SkinnedMeshAligner.AlignBindPose(prefabMesh, resultingSkinnedMesh);
+			Mesh resultingMesh = resultingSkinnedMesh.sharedMesh;
+			if (prefabMesh != null)
+			{
+				resultingMesh = SeamRemoval.PerformSeamRemoval(resultingSkinnedMesh, prefabMesh, 0.0001f);
+				resultingSkinnedMesh.sharedMesh = resultingMesh;
+				SkinnedMeshAligner.AlignBindPose(prefabMesh, resultingSkinnedMesh);
+			}
+
+			var bones = resultingSkinnedMesh.bones;
+			var usedBonesDictionary = CompileUsedBonesDictionary(resultingMesh);
+			if (usedBonesDictionary.Count != bones.Length)
+			{
+				resultingSkinnedMesh.bones = BuildNewReducedBonesList(bones, usedBonesDictionary);
+				resultingMesh = BuildNewReduceBonesMesh(resultingMesh, usedBonesDictionary);
+			}
 			
 			AssetDatabase.CreateAsset(resultingMesh, slotFolder + '/' + assetName + '/' + mesh.name + ".asset");
 					
@@ -256,5 +268,95 @@ namespace UMAEditor
 			AssetDatabase.SaveAssets();
 	        return slot;
 	    }
+
+		private static Mesh BuildNewReduceBonesMesh(Mesh sourceMesh, Dictionary<int, int> usedBonesDictionary)
+		{
+			var newMesh = new Mesh();
+			newMesh.vertices = sourceMesh.vertices;
+			newMesh.uv = sourceMesh.uv;
+			newMesh.uv2 = sourceMesh.uv2;
+			newMesh.uv3 = sourceMesh.uv3;
+			newMesh.uv4 = sourceMesh.uv4;
+			newMesh.tangents = sourceMesh.tangents;
+			newMesh.normals = sourceMesh.normals;
+			newMesh.name = sourceMesh.name;
+			newMesh.colors32 = sourceMesh.colors32;
+			newMesh.colors = sourceMesh.colors;
+			newMesh.boneWeights = BuildNewBoneWeights(sourceMesh.boneWeights, usedBonesDictionary);
+			newMesh.bindposes = BuildNewBindPoses(sourceMesh.bindposes, usedBonesDictionary);
+			for (int i = 0; i < sourceMesh.subMeshCount; i++)
+			{
+				newMesh.SetTriangles(i, sourceMesh.GetTriangles(i));
+			}
+			return newMesh;
+		}
+
+		private static Matrix4x4[] BuildNewBindPoses(Matrix4x4[] bindPoses, Dictionary<int, int> usedBonesDictionary)
+		{
+			var res = new Matrix4x4[usedBonesDictionary.Count];
+			foreach (var entry in usedBonesDictionary)
+			{
+				res[entry.Value] = bindPoses[entry.Key];
+			}
+			return res;
+		}
+
+		private static BoneWeight[] BuildNewBoneWeights(BoneWeight[] boneWeight, Dictionary<int, int> usedBonesDictionary)
+		{
+			var res = new BoneWeight[boneWeight.Length];
+			for(int i = 0; i < boneWeight.Length; i++ )
+			{
+				UpdateBoneWeight(ref boneWeight[i], ref res[i], usedBonesDictionary);
+			}
+			return res;
+		}
+
+		private static void UpdateBoneWeight(ref BoneWeight source, ref BoneWeight dest, Dictionary<int, int> usedBonesDictionary)
+		{
+			dest.weight0 = source.weight0;
+			dest.weight1 = source.weight1;
+			dest.weight2 = source.weight2;
+			dest.weight3 = source.weight3;
+			dest.boneIndex0 = UpdateBoneIndex(source.boneIndex0, usedBonesDictionary);
+			dest.boneIndex1 = UpdateBoneIndex(source.boneIndex1, usedBonesDictionary);
+			dest.boneIndex2 = UpdateBoneIndex(source.boneIndex2, usedBonesDictionary);
+			dest.boneIndex3 = UpdateBoneIndex(source.boneIndex3, usedBonesDictionary);
+		}
+
+		private static int UpdateBoneIndex(int boneIndex, Dictionary<int, int> usedBonesDictionary)
+		{
+			int res;
+			if( usedBonesDictionary.TryGetValue(boneIndex, out res)) return res;
+			return -1;
+		}
+
+		private static Transform[] BuildNewReducedBonesList(Transform[] bones, Dictionary<int, int> usedBonesDictionary)
+		{
+			var res = new Transform[usedBonesDictionary.Count];
+			foreach (var entry in usedBonesDictionary)
+			{
+				res[entry.Value] = bones[entry.Key];
+			}
+			return res;
+		}
+
+		private static Dictionary<int,int> CompileUsedBonesDictionary(Mesh resultingMesh)
+		{
+			var res = new Dictionary<int, int>();
+			var boneWeights = resultingMesh.boneWeights;			
+			foreach (var boneWeight in boneWeights)
+			{
+				AddBoneWeightToUsedBones(res, ref boneWeight);
+			}
+			return res;			
+		}
+
+		private static void AddBoneWeightToUsedBones(Dictionary<int, int> res, ref BoneWeight boneWeight)
+		{
+			if (boneWeight.weight0 > 0 && !res.ContainsKey(boneWeight.boneIndex0)) res.Add(boneWeight.boneIndex0, res.Count);
+			if (boneWeight.weight1 > 0 && !res.ContainsKey(boneWeight.boneIndex1)) res.Add(boneWeight.boneIndex1, res.Count);
+			if (boneWeight.weight2 > 0 && !res.ContainsKey(boneWeight.boneIndex2)) res.Add(boneWeight.boneIndex2, res.Count);
+			if (boneWeight.weight3 > 0 && !res.ContainsKey(boneWeight.boneIndex3)) res.Add(boneWeight.boneIndex3, res.Count);
+		}
 	}
 }
