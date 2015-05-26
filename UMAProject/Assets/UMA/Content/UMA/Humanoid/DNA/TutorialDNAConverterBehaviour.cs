b60182c5a2d6b893f739275e62401230e9c4df2c
ecurtz <eli@nuprometheus.com>
2015-05-26 14:23:14 -0700

    Updated Tutorial DNA to use SetPositionRelative()

--- UMAProject/Assets/UMA/Content/UMA/Humanoid/DNA/TutorialDNAConverterBehaviour.cs
+++ UMAProject/Assets/UMA/Content/UMA/Humanoid/DNA/TutorialDNAConverterBehaviour.cs
@@ -23,13 +23,8 @@ public class TutorialDNAConverterBehaviour : DnaConverterBehaviour
 		var umaDna = umaData.GetDna<UMADnaTutorial>();
 
 		float spacing = (umaDna.eyeSpacing - 0.5f) * 0.01f;
-
-		skeleton.SetPosition(UMAUtils.StringToHash("LeftEye"),
-							skeleton.GetPosition(UMAUtils.StringToHash("LeftEye")) +
-							new Vector3(0f, -spacing, 0f));
-		skeleton.SetPosition(UMAUtils.StringToHash("RightEye"),
-							skeleton.GetPosition(UMAUtils.StringToHash("RightEye")) +
-							new Vector3(0f, spacing, 0f));
-
-    }
+		
+		skeleton.SetPositionRelative(UMAUtils.StringToHash("LeftEye"), new Vector3(0f, -spacing, 0f));
+		skeleton.SetPositionRelative(UMAUtils.StringToHash("RightEye"), new Vector3(0f, spacing, 0f));
+	}
 }
