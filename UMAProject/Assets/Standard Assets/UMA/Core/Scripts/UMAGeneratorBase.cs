b110dae3571bda2f287e2d6fc8779c03f37e0faa
Yuuki <Yuuki_Minoru@live.com.au>
2015-07-10 22:34:40 +1000

    Fixed exposed var (textureMerge)

--- UMAProject/Assets/Standard Assets/UMA/Core/Scripts/UMAGeneratorBase.cs
+++ UMAProject/Assets/Standard Assets/UMA/Core/Scripts/UMAGeneratorBase.cs
@@ -12,6 +12,7 @@ namespace UMA
 	public abstract class UMAGeneratorBase : MonoBehaviour
 	{
 		public bool fitAtlas;
+		[HideInInspector]
 		public TextureMerge textureMerge;
 		public bool convertRenderTexture;
 		public bool convertMipMaps;
