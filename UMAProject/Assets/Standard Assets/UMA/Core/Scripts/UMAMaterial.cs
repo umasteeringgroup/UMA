2d3eeff985c57f674e1becf05d5fca45763439cb
davidosullivan <mail@david-osullivan.co.uk>
2016-03-30 00:21:48 +0100

    Adds UMAMaterial.Equals
    This checks if UMAMaterials are effectively equal, useful when using
    asset bundles, since if a slot.overlay uses a material that is in the
    binary, the materials get a different ID

--- UMAProject/Assets/Standard Assets/UMA/Core/Scripts/UMAMaterial.cs
+++ UMAProject/Assets/Standard Assets/UMA/Core/Scripts/UMAMaterial.cs
@@ -4,36 +4,36 @@ using System;
 
 namespace UMA
 {
-	/// <summary>
-	/// UMA wrapper for Unity material.
-	/// </summary>
-	public class UMAMaterial : ScriptableObject
-	{
-		public Material material;
-		public MaterialType materialType = MaterialType.Atlas;
-		public MaterialChannel[] channels;
+    /// <summary>
+    /// UMA wrapper for Unity material.
+    /// </summary>
+    public class UMAMaterial : ScriptableObject
+    {
+        public Material material;
+        public MaterialType materialType = MaterialType.Atlas;
+        public MaterialChannel[] channels;
 
-		public enum MaterialType
-		{
-			Atlas = 1,
-			NoAtlas = 2,
-		}
+        public enum MaterialType
+        {
+            Atlas = 1,
+            NoAtlas = 2,
+        }
 
-		public enum ChannelType
-		{
-			Texture = 0,
-			NormalMap = 1,
-			MaterialColor = 2,
-			TintedTexture = 3,
-		}
+        public enum ChannelType
+        {
+            Texture = 0,
+            NormalMap = 1,
+            MaterialColor = 2,
+            TintedTexture = 3,
+        }
 
-		[Serializable]
-		public struct MaterialChannel
-		{
-			public ChannelType channelType;
-			public RenderTextureFormat textureFormat;
-			public string materialPropertyName;
-		}
+        [Serializable]
+        public struct MaterialChannel
+        {
+            public ChannelType channelType;
+            public RenderTextureFormat textureFormat;
+            public string materialPropertyName;
+        }
 
 #if UNITY_EDITOR
 		[UnityEditor.MenuItem("Assets/Create/UMA Material")]
@@ -42,5 +42,32 @@ namespace UMA
 			UMAEditor.CustomAssetUtility.CreateAsset<UMAMaterial>();
 		}
 #endif
-	}
+        /// <summary>
+        /// Checks if UMAMaterials are effectively equal. Useful when comparing materials from asset bundles, that would otherwise say they are different to ones in the binary
+        /// </summary>
+        /// <param name="material">The material to compare</param>
+        /// <returns></returns>
+        public bool Equals(UMAMaterial material)
+        {
+            if (this.GetInstanceID() == material.GetInstanceID())
+            {
+                return true;
+            }
+            else
+            {
+                if (this.name == material.name &&
+                    this.material.name == material.material.name &&
+                    this.materialType == material.materialType &&
+                    this.channels.Length == material.channels.Length)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
 }
