53ab2f8d9143e8e5e27ca646f466db7093b026c9
davidosullivan <mail@david-osullivan.co.uk>
2016-12-02 16:36:39 +0000

    Another tweak to DynamicCharacterAvatar Inspector. Moved most of the 'standard' DynamicAvatar fields into 'Advanced Options' foldout. 'CharacterCreated/Updated/Destroyed are now in the 'Character Events' foldout.Nothing changes regards usage, it just de-clutters the inspector.

--- UMAProject/Assets/UMA/Extensions/DynamicCharacterSystem/Scripts/Editor/DynamicCharacterAvatarEditor.cs
+++ UMAProject/Assets/UMA/Extensions/DynamicCharacterSystem/Scripts/Editor/DynamicCharacterAvatarEditor.cs
@@ -29,12 +29,17 @@ public partial class DynamicCharacterAvatarEditor : Editor
     protected bool characterAvatarLoadSaveOpen;
     public override void OnInspectorGUI()
     {
-        Editor.DrawPropertiesExcluding(serializedObject, new string[] { "activeRace","preloadWardrobeRecipes", "raceAnimationControllers", "characterColors", "loadString", "loadFileOnStart", "loadPathType", "loadPath", "loadFilename","loadOptions", "savePathType", "savePath", "saveFilename", "makeUnique", "BoundsOffset" });
+        Editor.DrawPropertiesExcluding(serializedObject, new string[] { "activeRace","preloadWardrobeRecipes", "raceAnimationControllers",
+			"characterColors", "loadString", "loadFileOnStart", "loadPathType", "loadPath", "loadFilename","loadOptions", "savePathType",
+			"savePath", "saveFilename", "makeUnique", "BoundsOffset",
+			/*Moved into AdvancedOptions*/"context","umaData","umaRecipe", "umaAdditionalRecipes","umaGenerator", "animationController",
+			/*Moved into CharacterEvents*/"CharacterCreated", "CharacterUpdated", "CharacterDestroyed", "RecipeUpdated" });
         serializedObject.ApplyModifiedProperties();
         SerializedProperty thisRaceSetter = serializedObject.FindProperty("activeRace");
         Rect currentRect = EditorGUILayout.GetControlRect(false, _racePropDrawer.GetPropertyHeight(thisRaceSetter, GUIContent.none));
 		_racePropDrawer.OnGUI(currentRect, thisRaceSetter, new GUIContent(thisRaceSetter.displayName));
 		//Other DCA propertyDrawers
+		GUILayout.Space(2f);
 		EditorGUI.BeginChangeCheck();
 		EditorGUILayout.PropertyField(serializedObject.FindProperty("preloadWardrobeRecipes"));
 		if (EditorGUI.EndChangeCheck())
@@ -58,14 +63,19 @@ public partial class DynamicCharacterAvatarEditor : Editor
 				thisDCA.SetAnimatorController();
 			}
 		}
+		//GUILayout.Space(1f);
 		EditorGUI.BeginChangeCheck();
-		EditorGUILayout.PropertyField(serializedObject.FindProperty("characterColors"),true);
+		SerializedProperty characterColors = serializedObject.FindProperty("characterColors");
+		//Rect charColsRect = EditorGUILayout.GetControlRect();
+		//EditorGUI.PropertyField(charColsRect,characterColors.FindPropertyRelative("Colors"), new GUIContent("Character Colors"),true);
+		EditorGUILayout.PropertyField(characterColors.FindPropertyRelative("Colors"), new GUIContent("Character Colors"), true);
 		if (EditorGUI.EndChangeCheck())
 		{
 			serializedObject.ApplyModifiedProperties();
 			if (Application.isPlaying)
 				thisDCA.UpdateColors(true);
 		}
+		GUILayout.Space(2f);
 		//Load save fields
 		EditorGUI.BeginChangeCheck();
 		SerializedProperty loadPathType = serializedObject.FindProperty("loadPathType");
@@ -81,10 +91,10 @@ public partial class DynamicCharacterAvatarEditor : Editor
             SerializedProperty savePath = serializedObject.FindProperty("savePath");
             SerializedProperty saveFilename = serializedObject.FindProperty("saveFilename");
             SerializedProperty makeUnique = serializedObject.FindProperty("makeUnique");
-            EditorGUILayout.PropertyField(loadPathType);
 
+			EditorGUILayout.PropertyField(loadPathType);
 
-            if (loadPathType.enumValueIndex == Convert.ToInt32(DynamicCharacterAvatar.loadPathTypes.String))
+			if (loadPathType.enumValueIndex == Convert.ToInt32(DynamicCharacterAvatar.loadPathTypes.String))
             {
                 EditorGUILayout.PropertyField(loadString);
             }
@@ -132,13 +142,61 @@ public partial class DynamicCharacterAvatarEditor : Editor
         {
             serializedObject.ApplyModifiedProperties();
         }
-        EditorGUI.BeginChangeCheck();
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("BoundsOffset"));
+		GUILayout.Space(2f);
+		//for CharacterEvents
+		EditorGUI.BeginChangeCheck();
+		SerializedProperty CharacterCreated = serializedObject.FindProperty("CharacterCreated");
+		CharacterCreated.isExpanded = EditorGUILayout.Foldout(CharacterCreated.isExpanded, "Character Events");
+		if (CharacterCreated.isExpanded)
+		{
+			SerializedProperty CharacterUpdated = serializedObject.FindProperty("CharacterUpdated");
+			SerializedProperty CharacterDestroyed= serializedObject.FindProperty("CharacterDestroyed");
+			SerializedProperty RecipeUpdated = serializedObject.FindProperty("RecipeUpdated");
+
+			EditorGUILayout.PropertyField(CharacterCreated);
+			EditorGUILayout.PropertyField(CharacterUpdated);
+			EditorGUILayout.PropertyField(CharacterDestroyed);
+			EditorGUILayout.PropertyField(RecipeUpdated);
+		}
+		if (EditorGUI.EndChangeCheck())
+		{
+			serializedObject.ApplyModifiedProperties();
+		}
+		GUILayout.Space(2f);
+		//for AdvancedOptions
+		EditorGUI.BeginChangeCheck();
+		SerializedProperty context = serializedObject.FindProperty("context");
+		context.isExpanded = EditorGUILayout.Foldout(context.isExpanded, "Advanced Options");
+		if (context.isExpanded)
+		{
+			SerializedProperty umaData = serializedObject.FindProperty("umaData");
+			SerializedProperty umaGenerator = serializedObject.FindProperty("umaGenerator");
+			SerializedProperty umaRecipe = serializedObject.FindProperty("umaRecipe");
+			SerializedProperty umaAdditionalRecipes = serializedObject.FindProperty("umaAdditionalRecipes");
+			SerializedProperty animationController = serializedObject.FindProperty("animationController");
+
+			EditorGUILayout.PropertyField(context);
+			EditorGUILayout.PropertyField(umaData);
+			EditorGUILayout.PropertyField(umaGenerator);
+			EditorGUILayout.Space();
+			EditorGUILayout.PropertyField(umaRecipe);
+			EditorGUILayout.PropertyField(umaAdditionalRecipes, true);
+			EditorGUILayout.PropertyField(animationController);
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("BoundsOffset"));
+		}
+		if (EditorGUI.EndChangeCheck())
+		{
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		/*EditorGUI.BeginChangeCheck();
+        
         if (EditorGUI.EndChangeCheck())
         {
             serializedObject.ApplyModifiedProperties();
-        }
-        if (Application.isPlaying)
+        }*/
+		GUILayout.Space(2f);
+		if (Application.isPlaying)
         {
             EditorGUILayout.LabelField("AssetBundles used by Avatar");
             SerializedProperty assetBundlesUsedbyCharacter = serializedObject.FindProperty("assetBundlesUsedbyCharacter");
