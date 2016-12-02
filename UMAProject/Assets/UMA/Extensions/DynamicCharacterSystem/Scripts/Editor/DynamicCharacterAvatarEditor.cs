887f28b8f48eb9f5a29c54242c342bbd3fd3b493
davidosullivan <mail@david-osullivan.co.uk>
2016-12-02 15:17:22 +0000

    Fixed loadOptions not showing up right in DCA inspector

--- UMAProject/Assets/UMA/Extensions/DynamicCharacterSystem/Scripts/Editor/DynamicCharacterAvatarEditor.cs
+++ UMAProject/Assets/UMA/Extensions/DynamicCharacterSystem/Scripts/Editor/DynamicCharacterAvatarEditor.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -29,7 +29,7 @@ public partial class DynamicCharacterAvatarEditor : Editor
     protected bool characterAvatarLoadSaveOpen;
     public override void OnInspectorGUI()
     {
-        Editor.DrawPropertiesExcluding(serializedObject, new string[] { "activeRace","preloadWardrobeRecipes", "raceAnimationControllers", "characterColors", "loadFileOnStart", "loadPathType", "loadPath", "loadFilename","waitForBundles", "savePathType", "savePath", "saveFilename", "makeUnique", "BoundsOffset", "umaRecipe" });
+        Editor.DrawPropertiesExcluding(serializedObject, new string[] { "activeRace","preloadWardrobeRecipes", "raceAnimationControllers", "characterColors", "loadFileOnStart", "loadPathType", "loadPath", "loadFilename","loadOptions", "savePathType", "savePath", "saveFilename", "makeUnique", "BoundsOffset", "umaRecipe" });
         serializedObject.ApplyModifiedProperties();
         SerializedProperty thisRaceSetter = serializedObject.FindProperty("activeRace");
         Rect currentRect = EditorGUILayout.GetControlRect(false, _racePropDrawer.GetPropertyHeight(thisRaceSetter, GUIContent.none));
@@ -76,7 +76,7 @@ public partial class DynamicCharacterAvatarEditor : Editor
             SerializedProperty loadPath = serializedObject.FindProperty("loadPath");
             SerializedProperty loadFilename = serializedObject.FindProperty("loadFilename");
             SerializedProperty loadFileOnStart = serializedObject.FindProperty("loadFileOnStart");
-            SerializedProperty waitForBundles = serializedObject.FindProperty("waitForBundles");
+            SerializedProperty loadOptions = serializedObject.FindProperty("loadOptions");
             SerializedProperty savePathType = serializedObject.FindProperty("savePathType");
             SerializedProperty savePath = serializedObject.FindProperty("savePath");
             SerializedProperty saveFilename = serializedObject.FindProperty("saveFilename");
@@ -101,7 +101,7 @@ public partial class DynamicCharacterAvatarEditor : Editor
             {
                 EditorGUILayout.PropertyField(loadFileOnStart);
             }
-            EditorGUILayout.PropertyField(waitForBundles);
+            EditorGUILayout.PropertyField(loadOptions, true);
             if (Application.isPlaying)
             {
                 if (GUILayout.Button("Perform Load"))
