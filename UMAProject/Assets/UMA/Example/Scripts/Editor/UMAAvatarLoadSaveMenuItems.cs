dafd611a8e58ef19cf329549bcbcf1dc10d2149c
davidosullivan <mail@david-osullivan.co.uk>
2016-12-16 05:03:39 +0000

    Big Update to DCS Load/Save/RecipeModel part1

--- UMAProject/Assets/UMA/Example/Scripts/Editor/UMAAvatarLoadSaveMenuItems.cs
+++ UMAProject/Assets/UMA/Example/Scripts/Editor/UMAAvatarLoadSaveMenuItems.cs
@@ -1,102 +1,101 @@
 using UnityEngine;
 using UnityEditor;
 using UMA;
-using System;
 
 public class UMAAvatarLoadSaveMenuItems : Editor
 {
-    [MenuItem("UMA/Runtime/Save Selected Avatars generated textures to PNG")]
-    public static void SaveSelectedAvatarsPNG()
-    {
-        if (!Application.isPlaying)
-        {
-            EditorUtility.DisplayDialog("Notice", "This function is only available at runtime", "Got it");
-            return;
-        }
+	[MenuItem("UMA/Runtime/Save Selected Avatars generated textures to PNG")]
+	public static void SaveSelectedAvatarsPNG()
+	{
+		if (!Application.isPlaying)
+		{
+			EditorUtility.DisplayDialog("Notice", "This function is only available at runtime", "Got it");
+			return;
+		}
 
-        if (Selection.gameObjects.Length != 1)
-        {
-            EditorUtility.DisplayDialog("Notice", "Only one Avatar can be selected.", "OK");
-            return;
-        }
+		if (Selection.gameObjects.Length != 1)
+		{
+			EditorUtility.DisplayDialog("Notice", "Only one Avatar can be selected.", "OK");
+			return;
+		}
 
-        var selectedTransform = Selection.gameObjects[0].transform;
-        var avatar = selectedTransform.GetComponent<UMAAvatarBase>();
+		var selectedTransform = Selection.gameObjects[0].transform;
+		var avatar = selectedTransform.GetComponent<UMAAvatarBase>();
 
-        if (avatar == null)
-        {
-            EditorUtility.DisplayDialog("Notice", "An Avatar must be selected to use this function", "OK");
-            return;
-        }
+		if (avatar == null)
+		{
+			EditorUtility.DisplayDialog("Notice", "An Avatar must be selected to use this function", "OK");
+			return;
+		}
 
-        SkinnedMeshRenderer smr = avatar.gameObject.GetComponentInChildren<SkinnedMeshRenderer>();
-        if (smr == null) 
-        {
-            EditorUtility.DisplayDialog("Warning", "Could not find SkinnedMeshRenderer in Avatar hierarchy", "OK");
-            return;
-        }
+		SkinnedMeshRenderer smr = avatar.gameObject.GetComponentInChildren<SkinnedMeshRenderer>();
+		if (smr == null)
+		{
+			EditorUtility.DisplayDialog("Warning", "Could not find SkinnedMeshRenderer in Avatar hierarchy", "OK");
+			return;
+		}
 
-        string path = EditorUtility.SaveFilePanelInProject("Save Texture(s)", "Texture.png", "png", "Base Filename to save PNG files to.");
-        if (!string.IsNullOrEmpty(path))
-        {
-            string basename = System.IO.Path.GetFileNameWithoutExtension(path);
-            string pathname = System.IO.Path.GetDirectoryName(path);
-            // save the diffuse texture
-            for (int i=0;i<smr.materials.Length;i++)
-            {
-                string PathBase = System.IO.Path.Combine(pathname, basename + "_material_" + i.ToString());
-                string DiffuseName = PathBase + "_Diffuse.PNG";
-                SaveTexture(smr.materials[i].GetTexture("_MainTex"), DiffuseName);
-            }
-        }
-    }
+		string path = EditorUtility.SaveFilePanelInProject("Save Texture(s)", "Texture.png", "png", "Base Filename to save PNG files to.");
+		if (!string.IsNullOrEmpty(path))
+		{
+			string basename = System.IO.Path.GetFileNameWithoutExtension(path);
+			string pathname = System.IO.Path.GetDirectoryName(path);
+			// save the diffuse texture
+			for (int i = 0; i < smr.materials.Length; i++)
+			{
+				string PathBase = System.IO.Path.Combine(pathname, basename + "_material_" + i.ToString());
+				string DiffuseName = PathBase + "_Diffuse.PNG";
+				SaveTexture(smr.materials[i].GetTexture("_MainTex"), DiffuseName);
+			}
+		}
+	}
 
-    private static void SaveTexture(Texture texture, string diffuseName)
-    {
-        if (texture is RenderTexture)
-        {
-            SaveRenderTexture(texture as RenderTexture, diffuseName);
-            return;
-        }
-        else if (texture is Texture2D)
-        {
-            SaveTexture2D(texture as Texture2D, diffuseName);
-            return;
-        }
-        EditorUtility.DisplayDialog("Error", "Texture is not RenderTexture or Texture2D", "OK");
-    }
+	private static void SaveTexture(Texture texture, string diffuseName)
+	{
+		if (texture is RenderTexture)
+		{
+			SaveRenderTexture(texture as RenderTexture, diffuseName);
+			return;
+		}
+		else if (texture is Texture2D)
+		{
+			SaveTexture2D(texture as Texture2D, diffuseName);
+			return;
+		}
+		EditorUtility.DisplayDialog("Error", "Texture is not RenderTexture or Texture2D", "OK");
+	}
 
-    static public Texture2D GetRTPixels(RenderTexture rt)
-    {
+	static public Texture2D GetRTPixels(RenderTexture rt)
+	{
 
-        // Remember currently active render texture
-        RenderTexture currentActiveRT = RenderTexture.active;
+		// Remember currently active render texture
+		RenderTexture currentActiveRT = RenderTexture.active;
 
-        // Set the supplied RenderTexture as the active one
-        RenderTexture.active = rt;
+		// Set the supplied RenderTexture as the active one
+		RenderTexture.active = rt;
 
-        // Create a new Texture2D and read the RenderTexture image into it
-        Texture2D tex = new Texture2D(rt.width, rt.height);
-        tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+		// Create a new Texture2D and read the RenderTexture image into it
+		Texture2D tex = new Texture2D(rt.width, rt.height);
+		tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
 
-        // Restorie previously active render texture
-        RenderTexture.active = currentActiveRT;
-        return tex;
-    }
+		// Restorie previously active render texture
+		RenderTexture.active = currentActiveRT;
+		return tex;
+	}
 
-    private static void SaveRenderTexture(RenderTexture texture, string textureName)
-    {
-        Texture2D tex = GetRTPixels(texture);
-        SaveTexture2D(tex, textureName);
-    }
+	private static void SaveRenderTexture(RenderTexture texture, string textureName)
+	{
+		Texture2D tex = GetRTPixels(texture);
+		SaveTexture2D(tex, textureName);
+	}
 
-    private static void SaveTexture2D(Texture2D texture, string textureName)
-    {
-        byte[] data = texture.EncodeToPNG();
-        System.IO.File.WriteAllBytes(textureName, data);
-    }
+	private static void SaveTexture2D(Texture2D texture, string textureName)
+	{
+		byte[] data = texture.EncodeToPNG();
+		System.IO.File.WriteAllBytes(textureName, data);
+	}
 
-    [MenuItem("UMA/Load and Save/Save Selected Avatar(s) Txt", priority=1)]
+	[MenuItem("UMA/Load and Save/Save Selected Avatar(s) Txt", priority=1)]
 	public static void SaveSelectedAvatarsTxt()
 	{
 		for (int i = 0; i < Selection.gameObjects.Length; i++)
@@ -118,7 +117,7 @@ public class UMAAvatarLoadSaveMenuItems : Editor
 					//check if Avatar is DCS
 					if (avatar is UMACharacterSystem.DynamicCharacterAvatar)
 					{
-						asset.SaveCharacterSystem(avatar.umaData.umaRecipe, avatar.context, (avatar as UMACharacterSystem.DynamicCharacterAvatar).WardrobeRecipes);
+						asset.Save(avatar.umaData.umaRecipe, avatar.context, (avatar as UMACharacterSystem.DynamicCharacterAvatar).WardrobeRecipes, true);
 					}
 					else
 					{
@@ -153,7 +152,7 @@ public class UMAAvatarLoadSaveMenuItems : Editor
 					//check if Avatar is DCS
 					if (avatar is UMACharacterSystem.DynamicCharacterAvatar)
 					{
-						asset.SaveCharacterSystem(avatar.umaData.umaRecipe, avatar.context, (avatar as UMACharacterSystem.DynamicCharacterAvatar).WardrobeRecipes);
+						asset.Save(avatar.umaData.umaRecipe, avatar.context, (avatar as UMACharacterSystem.DynamicCharacterAvatar).WardrobeRecipes,true);
 					}
 					else
 					{
@@ -162,6 +161,7 @@ public class UMAAvatarLoadSaveMenuItems : Editor
 					AssetDatabase.CreateAsset(asset, path);
 					AssetDatabase.SaveAssets();
 					Debug.Log("Recipe size: " + asset.recipeString.Length + " chars");
+
 				}
 			}
 		}
@@ -249,34 +249,67 @@ public class UMAAvatarLoadSaveMenuItems : Editor
 		}
 	}
 
-
-    [MenuItem("UMA/Load and Save/Save Dynamic Character Avatar to JSON", priority = 1)]
-    public static void SaveSelectedAvatarsJSON()
+	//@jaimi this is the equivalent of your previous JSON save but the resulting file does not need a special load method
+    [MenuItem("UMA/Load and Save/Save DynamicCharacterAvatar(s) txt (optimized)", priority = 1)]
+    public static void SaveSelectedAvatarsDCSTxt()
     {
         if (!Application.isPlaying)
         {
             EditorUtility.DisplayDialog("Notice", "This function is only available at runtime", "Got it");
             return;
-        }
+		}
+		else
+		{
+			EditorUtility.DisplayDialog("Notice", "The optimized save type is only compatible with DynamicCharacterAvatar avatars (or child classes of)", "Continue");
+		}
 
-        for (int i = 0; i < Selection.gameObjects.Length; i++)
+		for (int i = 0; i < Selection.gameObjects.Length; i++)
         {
             var selectedTransform = Selection.gameObjects[i].transform;
             var avatar = selectedTransform.GetComponent<UMACharacterSystem.DynamicCharacterAvatar>();
 
             if (avatar != null)
             {
-                var path = EditorUtility.SaveFilePanel("Save DynamicCharacterAvatar to JSON Text", "Assets", avatar.name + ".json", "json");
+                var path = EditorUtility.SaveFilePanel("Save DynamicCharacterAvatar Optimized Text", "Assets", avatar.name + ".txt", "txt");
                 if (path.Length != 0)
                 {
-                    string json = avatar.ToJson();
-                    System.IO.File.WriteAllText(path, json);
+					avatar.DoSave(false, path);
                 }
             }
         }
     }
+	//@jaimi this is the equivalent of your previous JSON save but the resulting file does not need a special load method and the resulting asset can also be inspected and edited
+	[MenuItem("UMA/Load and Save/Save DynamicCharacterAvatar(s) asset (optimized)", priority = 1)]
+	public static void SaveSelectedAvatarsDCSAsset()
+	{
+		if (!Application.isPlaying)
+		{
+			EditorUtility.DisplayDialog("Notice", "This function is only available at runtime", "Got it");
+			return;
+		}
+		else
+		{
+			EditorUtility.DisplayDialog("Notice", "The optimized save type is only compatible with DynamicCharacterAvatar avatars (or child classes of)", "Continue");
+		}
 
-    [MenuItem("UMA/Load and Save/Load Dynamic Character Avatar From JSON", priority = 1)]
+		for (int i = 0; i < Selection.gameObjects.Length; i++)
+		{
+			var selectedTransform = Selection.gameObjects[i].transform;
+			var avatar = selectedTransform.GetComponent<UMACharacterSystem.DynamicCharacterAvatar>();
+
+			if (avatar != null)
+			{
+				var path = EditorUtility.SaveFilePanelInProject("Save DynamicCharacterAvatar Optimized Asset", avatar.name + ".asset", "asset", "Message 2");
+				if (path.Length != 0)
+				{
+					avatar.DoSave(true, path);
+				}
+			}
+		}
+	}
+
+	//We dont need this now pUMATextRecipe works out which model was used itself
+	/*[MenuItem("UMA/Load and Save/Load Dynamic Character Avatar From JSON", priority = 1)]
     public static void LoadSelectedAvatarsJSON()
     {
         if (!Application.isPlaying)
@@ -299,5 +332,5 @@ public class UMAAvatarLoadSaveMenuItems : Editor
                 }
             }
         }
-    }
+    }*/
 }
