d39b60fa9403c1d20c6283c28341b7af6b7912dc
Victor Hagsand <victor_hagsand@hotmail.com>
2017-02-01 14:20:19 -0500

    Add in Unity updating and download stats

---
+++ UMAProject/Assets/UMA/Editor/Versioning/AssetVersion.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Xml;
+
+namespace kode80.Versioning
+{
+	public class AssetVersion
+	{
+		public string Name { get; private set; }
+		public string Author { get; private set; }
+		public SemanticVersion Version { get; private set; }
+		public string Notes { get; private set; }
+		public Uri packageURI { get; private set; }
+		public Uri versionURI { get; private set; }
+
+		public static AssetVersion ParseXML( string xmlString)
+		{
+			XmlDocument xml = new XmlDocument();
+
+			try { xml.LoadXml( xmlString); }
+			catch( XmlException) { return null; }
+
+			XmlNode name = xml.SelectSingleNode( "asset/name");
+			XmlNode author = xml.SelectSingleNode( "asset/author");
+			XmlNode version = xml.SelectSingleNode( "asset/version");
+			XmlNode notes = xml.SelectSingleNode( "asset/notes");
+			XmlNode packageURI = xml.SelectSingleNode( "asset/package-uri");
+			XmlNode versionURI = xml.SelectSingleNode( "asset/version-uri");
+
+			if( name == null || 
+				author == null || 
+				version == null || 
+				notes == null || 
+				packageURI == null || 
+				versionURI == null) 
+			{
+				Debug.Log( "Error parsing Asset Version XML");
+				return null;
+			}
+
+			SemanticVersion semanticVersion = SemanticVersion.Parse( version.InnerText);
+			if( semanticVersion == null) {
+				Debug.Log( "Error parsing Semantic Version");
+				return null;
+			}
+
+			AssetVersion assetVersion = new AssetVersion();
+			assetVersion.Name = name.InnerText;
+			assetVersion.Author = author.InnerText;
+			assetVersion.Version = semanticVersion;
+			assetVersion.Notes = notes.InnerText;
+			assetVersion.packageURI = new Uri( packageURI.InnerText);
+			assetVersion.versionURI = new Uri( versionURI.InnerText);
+
+			return assetVersion;
+		}
+
+		public override string ToString()
+		{
+			return "Name: " + Name + "\n" +
+				"Author: " + Author + "\n" +
+				"Version: " + Version + "\n" +
+				"Notes: " + Notes + "\n" +
+				"PackageURI: " + packageURI + "\n" +
+				"VersionURI: " + versionURI;
+		}
+	}
+}
---
+++ UMAProject/Assets/UMA/Editor/Versioning/AssetVersionDownloader.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace kode80.Versioning
+{
+	public class AssetVersionDownloader 
+	{
+		public delegate void RemoteVersionDownloadFinished( AssetVersion local, AssetVersion remote);
+		public RemoteVersionDownloadFinished remoteVersionDownloadFinished;
+
+		public delegate void RemoteVersionDownloadFailed( AssetVersion local);
+		public RemoteVersionDownloadFailed remoteVersionDownloadFailed;
+
+		private WebClient _webClient;
+		private List<AssetVersion> _queue;
+		private AssetVersion _currentLocalVersion;
+		private List<Action> _mainThreadDelegates;
+
+		public AssetVersionDownloader()
+		{
+			_queue = new List<AssetVersion>();
+			_mainThreadDelegates = new List<Action>();
+			EditorApplication.update += MainThreadUpdate;
+            ServicePointManager.ServerCertificateValidationCallback += HandleServerCertificateValidation;
+        }
+
+		~AssetVersionDownloader()
+		{
+			EditorApplication.update -= MainThreadUpdate;
+            ServicePointManager.ServerCertificateValidationCallback -= HandleServerCertificateValidation;
+            CancelAll();
+		}
+
+        private static bool HandleServerCertificateValidation(object sender, X509Certificate cert, X509Chain chain, System.Net.Security.SslPolicyErrors error)
+        {
+            return true;
+        }
+
+
+        public void Add( AssetVersion local)
+		{
+			_queue.Add( local);
+			AttemptNextDownload();
+		}
+
+		public void CancelAll()
+		{
+			if( _webClient != null) {
+				_webClient.CancelAsync();
+			}
+
+			_mainThreadDelegates.Clear();
+			_queue.Clear();
+		}
+
+		private void MainThreadUpdate()
+		{
+			if( _mainThreadDelegates.Count > 0)
+			{
+				Action action = _mainThreadDelegates[0];
+				_mainThreadDelegates.RemoveAt( 0);
+				action.Invoke();
+			}
+		}
+
+		private void AttemptNextDownload()
+		{
+			if( _webClient == null && _queue.Count > 0)
+			{
+				_currentLocalVersion = _queue[0];
+				_queue.RemoveAt( 0);
+
+				using( _webClient = new WebClient())
+				{
+					_webClient.DownloadStringCompleted += WebClientCompleted;
+                    
+                    try {
+						_webClient.DownloadStringAsync( _currentLocalVersion.versionURI);
+					}
+					catch( Exception e) {
+                        Debug.Log("dl exception: " + e);
+						HandleFailedDownload();
+					}
+				}
+			}
+		}
+
+		private void WebClientCompleted(object sender, DownloadStringCompletedEventArgs e)
+		{
+			if( e.Cancelled || e.Error != null) 
+			{
+                if( e.Error != null)
+                {
+                    Debug.Log("dl complete error: " + e.Error);
+                }
+				HandleFailedDownload();
+			}
+			else 
+			{
+				AssetVersion remote = AssetVersion.ParseXML( e.Result);
+				if( remote == null) 
+				{
+					HandleFailedDownload();
+				}
+				else 
+				{
+					HandleFinishedDownload( remote);
+				}
+			}
+		}
+
+		private void HandleFinishedDownload( AssetVersion remote)
+		{
+			if( remoteVersionDownloadFinished != null) {
+				_mainThreadDelegates.Add( new Action( () => {
+					remoteVersionDownloadFinished( _currentLocalVersion, remote);
+
+					_currentLocalVersion = null;
+					_webClient = null;
+					AttemptNextDownload();
+				}));
+			}
+		}
+
+		private void HandleFailedDownload()
+		{
+			if( remoteVersionDownloadFailed != null) {
+				_mainThreadDelegates.Add( new Action( () => {
+					remoteVersionDownloadFailed( _currentLocalVersion);
+
+					_currentLocalVersion = null;
+					_webClient = null;
+					AttemptNextDownload();
+				}));
+			}
+		}
+	}
+}
