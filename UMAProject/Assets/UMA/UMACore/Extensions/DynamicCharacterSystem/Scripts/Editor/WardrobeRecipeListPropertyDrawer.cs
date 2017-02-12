eed5aae951d9f761673d35673bedc6b48709a525
davidosullivan <mail@david-osullivan.co.uk>
2017-02-12 22:49:57 +0000

    Make DefaultWardrobeRecipies in DCA warn you if a default recipe you assigned is not live

--- UMAProject/Assets/UMA/UMACore/Extensions/DynamicCharacterSystem/Scripts/Editor/WardrobeRecipeListPropertyDrawer.cs
+++ UMAProject/Assets/UMA/UMACore/Extensions/DynamicCharacterSystem/Scripts/Editor/WardrobeRecipeListPropertyDrawer.cs
@@ -1,6 +1,7 @@
 #if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
+using UMA;
 using UMACharacterSystem;
 
 [CustomPropertyDrawer (typeof(DynamicCharacterAvatar.WardrobeRecipeList))]
@@ -95,6 +96,10 @@ public class WardrobeRecipeListPropertyDrawer : PropertyDrawer {
 			valR = new Rect (valR.xMin, (valR.yMin + 50f +padding), valR.width, EditorGUIUtility.singleLineHeight);
 			var thisRecipesProp = property.FindPropertyRelative ("recipes");
 			float textFieldWidth = (valR.width - 20f);
+			var warningStyle = new GUIStyle(EditorStyles.miniButton);
+			warningStyle.contentOffset = new Vector2(0f, 0f);
+			warningStyle.fontStyle = FontStyle.Bold;
+			var currentTint = GUI.color;
 			for (int i = 0; i < thisRecipesProp.arraySize; i++) {
 				var valRBut = new Rect((textFieldWidth + 18f),(valR.yMax + padding), 20f, EditorGUIUtility.singleLineHeight);
 				valR = new Rect (valR.xMin, (valR.yMax + padding), textFieldWidth, EditorGUIUtility.singleLineHeight);
@@ -112,6 +117,16 @@ public class WardrobeRecipeListPropertyDrawer : PropertyDrawer {
                 }
                 EditorGUI.TextField(valR,thisElement.FindPropertyRelative("_recipeName").stringValue + " ("+compatibleRaces+")");
 				EditorGUI.EndDisabledGroup ();
+				if (DynamicAssetLoader.Instance)
+				{
+					if (!CheckRecipeAvailability(thisElement.FindPropertyRelative("_recipeName").stringValue))
+					{
+						var warningRect = new Rect((valRBut.xMin - 25f),valRBut.yMin,20f,valRBut.height);
+						GUI.color = new Color(255, 200, 0);
+                        GUI.Box(warningRect, new GUIContent("!", thisElement.FindPropertyRelative("_recipeName").stringValue + " was not in a Resources folder or an asset bundle. You need to add it to one of these to make it 'LIVE'"), warningStyle);
+						GUI.color = currentTint;
+					}
+				}
 				if (GUI.Button (valRBut, "X")) {
 					thisRecipesProp.DeleteArrayElementAtIndex(i);
 					thisRecipesProp.serializedObject.ApplyModifiedProperties ();
@@ -120,6 +135,28 @@ public class WardrobeRecipeListPropertyDrawer : PropertyDrawer {
 			DropAreaGUI (dropArea, thisRecipesProp);
 		}
 		EditorGUI.EndProperty ();
+	}
+	/// <summary>
+	/// with wardobeRecipes, DynamicCharacterSystem does not have a list of refrenced recipes like the other libraries
+	/// so the only way to get them is from DynamicAssetLoader (which is how DCS gets them) 
+	/// so they MUST be in an assetBundle or in Resources or there is no way of finding them
+	/// </summary>
+	/// <param name="recipeName"></param>
+	/// <returns></returns>
+	private bool CheckRecipeAvailability(string recipeName)
+	{
+		//with wardobeRecipes, DynamicCharacterSystem does not have a list of refrenced recipes like the other libraries
+		//so the only way to get them is from DynamicAssetLoader (which is how DCS gets them) 
+		//so the MUST be in an assetBundle or in Resources or there is no way of finding them
+		bool found = false;
+		DynamicAssetLoader.Instance.debugOnFail = false;
+		found = DynamicAssetLoader.Instance.AddAssets<UMAWardrobeRecipe>(true, true, true, "", "", null, recipeName, null);
+		if (!found)
+			found = DynamicAssetLoader.Instance.AddAssets<UMATextRecipe>(true, true, true, "","", null, recipeName, null);
+		if (!found)
+			found = DynamicAssetLoader.Instance.AddAssets<UMAWardrobeCollection>(true, true, true, "", "", null, recipeName, null);
+		DynamicAssetLoader.Instance.debugOnFail = true;
+		return found;
 	}
 }
 #endif
