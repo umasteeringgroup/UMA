af7b2a87aa21c00559e5b7ae968e3ac25838de6a
kenamis <kenan.chabuk@gmail.com>
2017-03-18 12:00:22 -0700

    Fix for losing mouse lock

--- UMAProject/Assets/UMA/Examples/Physics Examples/Scripts/CursorLock.cs
+++ UMAProject/Assets/UMA/Examples/Physics Examples/Scripts/CursorLock.cs
@@ -5,6 +5,17 @@ public class CursorLock : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+	}
+
+	void OnApplicationFocus(bool hasFocus )
+	{
+		if( hasFocus )
+			LockMouse ();
+	}
+
+	void LockMouse()
+	{
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 	}
