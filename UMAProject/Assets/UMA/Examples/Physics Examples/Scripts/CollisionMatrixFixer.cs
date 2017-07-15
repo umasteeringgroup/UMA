ba87abd50cda47d9d98734e688ac94db1196a4b4
kenamis <kenan.chabuk@gmail.com>
2017-07-15 12:13:54 -0700

    collisionMatrixFixer prefab created and added to ragdoll and cloth example scenes

---
+++ UMAProject/Assets/UMA/Examples/Physics Examples/Scripts/CollisionMatrixFixer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionMatrixFixer : MonoBehaviour 
+{
+    private int _defaultRagdollLayer = 8;
+    private int _defaultPlayerLayer = 9;
+
+	// Use this for initialization
+	void Start () 
+    {
+        for (int i = 8; i < 32; i++)
+        {
+            if( i != _defaultRagdollLayer )
+                Physics.IgnoreLayerCollision(_defaultRagdollLayer, i, true);
+        }	
+        Physics.IgnoreLayerCollision(_defaultRagdollLayer, _defaultRagdollLayer, false);
+	}
+}
