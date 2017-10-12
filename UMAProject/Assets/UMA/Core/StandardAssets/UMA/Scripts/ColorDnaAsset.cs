275a13cdc55c59ba5fe71ceb858f8bb6104a0caf
kenamis <kenan.chabuk@gmail.com>
2017-10-11 19:45:59 -0700

    added colorDna and example scene

---
+++ UMAProject/Assets/UMA/Core/StandardAssets/UMA/Scripts/ColorDnaAsset.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UMA
+{
+    public class ColorDnaAsset : ScriptableObject
+    {
+        [System.Serializable]
+        public class DNAColorSet
+        {
+            public string dnaEntryName;
+            public string overlayEntryName;
+            [Tooltip("Color Channel: For example PBR, 0 = Albedo, 1 = Normal, 2 = Metallic")]
+            public int colorChannel = 0;
+            public Color32 minColor = new Color(1,1,1,0);
+            public Color32 maxColor = new Color(1,1,1,1);
+        }
+
+        public int dnaTypeHash;
+       
+        public DNAColorSet[] colorSets;
+
+        void OnEnable()
+        {
+            if (colorSets == null)
+            {
+                colorSets = new DNAColorSet[0];
+            }
+        }
+    	
+        #if UNITY_EDITOR
+        [UnityEditor.MenuItem("Assets/Create/UMA/DNA/Color DNA")]
+        public static void CreateColorDnaAsset()
+        {
+            UMA.CustomAssetUtility.CreateAsset<ColorDnaAsset>();
+        }
+        #endif
+    }
+}
---
+++ UMAProject/Assets/UMA/Examples/Extensions Examples/ColorDNA/Scripts/ColorDnaSlider.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UMA;
+
+public class ColorDnaSlider : MonoBehaviour 
+{
+    public int dnaTypeHash = 1566835252;
+    public int dnaEntryIndex = 0;
+
+    protected UMAData data;
+    protected UMADnaBase dna;
+
+    public void OnCharacterCreated(UMAData umaData)
+    {
+        this.data = umaData;
+        Slider slider = gameObject.GetComponent<Slider>();
+
+        dna = umaData.GetDna(dnaTypeHash);      
+        if (dna != null)
+        {
+            slider.value = dna.GetValue(dnaEntryIndex);
+        }
+    }
+
+    public void SetColorDNA(float value)
+    {
+        if (dna == null)
+        {
+            dna = data.GetDna(dnaTypeHash);     
+        }
+
+        if (dna != null)
+        {
+            dna.SetValue(dnaEntryIndex, value);
+            data.Dirty(true, true, false);
+        }
+    }
+}
