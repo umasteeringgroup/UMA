83393944f54e90f5c5ec60e2bca553fa0a1c27fd
kenamis <kenan.chabuk@gmail.com>
2018-01-05 18:05:27 -0800

    moved classes into a namespace

--- UMAProject/Assets/UMA/Examples/Physics Examples/Scripts/ClothAccelerationPanelScript.cs
+++ UMAProject/Assets/UMA/Examples/Physics Examples/Scripts/ClothAccelerationPanelScript.cs
@@ -1,38 +1,42 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
-using UMA;
 using UMA.CharacterSystem;
 
-public class ClothAccelerationPanelScript : MonoBehaviour 
+// @cond doxygen ignore
+namespace UMA.Examples
 {
-    public DynamicCharacterAvatar avatar;
-    public Slider xSlider;
-    public Slider ySlider;
-    public Slider zSlider;
+	public class ClothAccelerationPanelScript : MonoBehaviour 
+	{
+	    public DynamicCharacterAvatar avatar;
+	    public Slider xSlider;
+	    public Slider ySlider;
+	    public Slider zSlider;
 
-    private Cloth m_Cloth;
-    private Vector3 acceleration;
+	    private Cloth m_Cloth;
+	    private Vector3 acceleration;
 
-    public void UpdateClothAcceleration()
-    {
-        if (avatar == null)
-            return;
+	    public void UpdateClothAcceleration()
+	    {
+	        if (avatar == null)
+	            return;
 
-        if (xSlider == null || ySlider == null || zSlider == null)
-            return;
+	        if (xSlider == null || ySlider == null || zSlider == null)
+	            return;
 
-        if (m_Cloth == null)
-        {
-            m_Cloth = avatar.GetComponentInChildren<Cloth>();
-            if (m_Cloth == null)
-                return;
-        }
+	        if (m_Cloth == null)
+	        {
+	            m_Cloth = avatar.GetComponentInChildren<Cloth>();
+	            if (m_Cloth == null)
+	                return;
+	        }
 
-        acceleration.x = xSlider.value;
-        acceleration.y = ySlider.value;
-        acceleration.z = zSlider.value;
+	        acceleration.x = xSlider.value;
+	        acceleration.y = ySlider.value;
+	        acceleration.z = zSlider.value;
 
-        m_Cloth.externalAcceleration = acceleration;
-    }
+	        m_Cloth.externalAcceleration = acceleration;
+	    }
+	}
 }
+// @endcond
