e39e88f1173671252dcd884f9b9b82e9696b9454
kenamis <kenan.chabuk@gmail.com>
2018-01-08 15:01:15 -0800

    fixed blendshape baking early out check

--- UMAProject/Assets/UMA/Core/StandardAssets/UMA/Scripts/SkinnedMeshCombiner.cs
+++ UMAProject/Assets/UMA/Core/StandardAssets/UMA/Scripts/SkinnedMeshCombiner.cs
@@ -208,10 +208,11 @@ namespace UMA
 						for (int shapeIndex = 0; shapeIndex < source.meshData.blendShapes.Length; shapeIndex++)
 						{
                             #region BlendShape Baking
-                            if(blendShapeSettings.bakeBlendShapes != null || blendShapeSettings.bakeBlendShapes.Count == 0)
+                            if(blendShapeSettings.bakeBlendShapes != null && blendShapeSettings.bakeBlendShapes.Count >= 0)
                             {
                                 // If there are names in the bakeBlendShape dictionary and we find them in the meshData blendshape list, then lets bake them instead of adding them.
                                 UMABlendShape currentShape = source.meshData.blendShapes[shapeIndex];
+
                                 if( blendShapeSettings.bakeBlendShapes.ContainsKey(currentShape.shapeName))
                                 {
                                     float weight = blendShapeSettings.bakeBlendShapes[currentShape.shapeName] * 100.0f;
