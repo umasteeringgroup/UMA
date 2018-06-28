898236e304ffa32faf5e7011b2915cb83f569eaa
kenamis <kenan.chabuk@gmail.com>
2018-06-28 14:23:20 -0700

    moved "AddDefaultLayers" to UMAPhysicsAvatarEditor and made static

--- UMAProject/Assets/UMA/Core/StandardAssets/Extensions/UMAPhysics/Editor/UMAPhysicsSlotEditor.cs
+++ UMAProject/Assets/UMA/Core/StandardAssets/Extensions/UMAPhysics/Editor/UMAPhysicsSlotEditor.cs
@@ -24,7 +24,7 @@ namespace UMA.Dynamics.Editors
 			EditorGUILayout.HelpBox ("Sets layer 8 and 9 to Ragdoll and Player. If your code uses different layers do not use this defaults button", MessageType.Info);
 			if (GUILayout.Button ("Add Default Layers")) 
 			{
-				AddDefaultLayers ();
+				UMAPhysicsAvatarEditor.AddDefaultLayers (ragdollLayer, playerLayer);
 			}
 			EditorGUILayout.HelpBox ("The Ragdoll layer needs it's collision matrix layers set to collide with only itself. Set this in Edit->Project Settings->Physics->Layer Collision Matrix", MessageType.Info);
 
@@ -32,51 +32,6 @@ namespace UMA.Dynamics.Editors
 			playerLayer.intValue = EditorGUILayout.LayerField ("Player Layer", playerLayer.intValue);
 
 			serializedObject.ApplyModifiedProperties();
-		}
-
-		private void AddDefaultLayers()
-		{
-			CreateLayer ("Ragdoll");
-			CreateLayer ("Player");
-
-			for (int i = 8; i < 32; i++)
-			{
-				if( i != ragdollLayer.intValue )
-				Physics.IgnoreLayerCollision(ragdollLayer.intValue, i, true);
-			}
-
-			Physics.IgnoreLayerCollision(ragdollLayer.intValue, ragdollLayer.intValue, false);
-		}
-
-		private void CreateLayer(string name)
-		{
-			//  https://forum.unity.com/threads/adding-layer-by-script.41970/#post-2274824
-			SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
-			SerializedProperty layers = tagManager.FindProperty("layers");
-			bool ExistLayer = false;
-
-			for (int i = 8; i < layers.arraySize; i++)
-			{
-				SerializedProperty layerSP = layers.GetArrayElementAtIndex(i);
-
-				if (layerSP.stringValue == name)
-				{
-					ExistLayer = true;
-					break;
-				}
-
-			}
-			for (int j = 8; j < layers.arraySize; j++)
-			{
-				SerializedProperty layerSP = layers.GetArrayElementAtIndex(j);
-				if (layerSP.stringValue == "" && !ExistLayer)
-				{
-					layerSP.stringValue = name;
-					tagManager.ApplyModifiedProperties();
-
-					break;
-				}
-			}
 		}
 	}
 }
