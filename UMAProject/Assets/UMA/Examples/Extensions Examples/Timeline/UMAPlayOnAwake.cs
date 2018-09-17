2358eef164bf4a8f8e5102699df38d8b508ee185
kenamis <kenan.chabuk@gmail.com>
2018-09-17 14:40:28 -0700

    added script to play on uma creation

---
+++ UMAProject/Assets/UMA/Examples/Extensions Examples/Timeline/UMAPlayOnAwake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UMA;
+using UMA.CharacterSystem;
+using UnityEngine.Playables;
+
+namespace UMA.Examples
+{
+    public class UMAPlayOnAwake : MonoBehaviour
+    {
+        public PlayableDirector playableDirector;
+
+        DynamicCharacterAvatar avatar;
+
+        // Use this for initialization
+        void Start()
+        {
+            avatar = GetComponent<DynamicCharacterAvatar>();
+            avatar.CharacterCreated.AddListener(OnCharacterCreated);
+        }
+
+        public void OnCharacterCreated(UMAData umaData)
+        {
+            playableDirector.Play();
+        }
+    }
+}
