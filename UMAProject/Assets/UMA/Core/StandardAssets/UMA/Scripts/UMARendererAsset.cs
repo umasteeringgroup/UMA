453d4e8e3e63403acdaf13276f0903dcecbc3214
kenamis <kenan.chabuk@gmail.com>
2019-01-31 20:32:38 -0800

    encapsulated private variables and public properties for RendererAsset

--- UMAProject/Assets/UMA/Core/StandardAssets/UMA/Scripts/UMARendererAsset.cs
+++ UMAProject/Assets/UMA/Core/StandardAssets/UMA/Scripts/UMARendererAsset.cs
@@ -6,22 +6,29 @@ namespace UMA
 {
     public class UMARendererAsset : ScriptableObject
     {
-        public string RendererName;
-        public bool UpdateWhenOffscreen = false;
-        public bool SkinnedMotionVectors = false;
+        public string RendererName { get { return _RendererName; } }
+        public bool UpdateWhenOffscreen { get { return _UpdateWhenOffscreen; } }
+        public bool SkinnedMotionVectors { get { return _SkinnedMotionVectors; } }
+        public UnityEngine.Rendering.ShadowCastingMode CastShadows { get { return _CastShadows; } }
+        public bool ReceiveShadows { get { return _ReceiveShadows; } }
+
+        [SerializeField] private string _RendererName;
+        [SerializeField] private bool _UpdateWhenOffscreen = false;
+        [SerializeField] private bool _SkinnedMotionVectors = false;
+
         [Header("Lighting")]
-        public UnityEngine.Rendering.ShadowCastingMode CastShadows = UnityEngine.Rendering.ShadowCastingMode.On;
-        public bool ReceiveShadows = true;
+        [SerializeField] private UnityEngine.Rendering.ShadowCastingMode _CastShadows = UnityEngine.Rendering.ShadowCastingMode.On;
+        [SerializeField] private bool _ReceiveShadows = true;
 
         public void ApplySettingsToRenderer(SkinnedMeshRenderer smr)
         {
             if(!string.IsNullOrEmpty(RendererName))
                 smr.name = RendererName;
 
-            smr.updateWhenOffscreen = UpdateWhenOffscreen;
-            smr.skinnedMotionVectors = SkinnedMotionVectors;
-            smr.shadowCastingMode = CastShadows;
-            smr.receiveShadows = ReceiveShadows;
+            smr.updateWhenOffscreen = _UpdateWhenOffscreen;
+            smr.skinnedMotionVectors = _SkinnedMotionVectors;
+            smr.shadowCastingMode = _CastShadows;
+            smr.receiveShadows = _ReceiveShadows;
         }
 
 #if UNITY_EDITOR
