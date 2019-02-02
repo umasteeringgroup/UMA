92462196bdc910331ad32ebb87ce922aa5d58aee
kenamis <kenan.chabuk@gmail.com>
2019-02-02 13:54:53 -0800

    added DCARendererManager and inspector

---
+++ UMAProject/Assets/UMA/Core/Extensions/DynamicCharacterSystem/Scripts/DCARendererManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace UMA.CharacterSystem
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(DynamicCharacterAvatar))]
+    public class DCARendererManager : MonoBehaviour
+    {
+        [System.Serializable]
+        public class RendererElement
+        {
+            public UMARendererAsset rendererAsset;
+            public List<SlotDataAsset> slotAssets = new List<SlotDataAsset>();
+            public List<string> wardrobeSlots = new List<string>();
+        }
+        public List<RendererElement> RendererElements = new List<RendererElement>();
+
+        private DynamicCharacterAvatar avatar;
+        private UMAData.UMARecipe umaRecipe = new UMAData.UMARecipe();
+        List<SlotDataAsset> wardrobeSlotAssets = new List<SlotDataAsset>();
+        private UMAContext context;
+
+        // Use this for initialization
+        void Start()
+        {
+            avatar = GetComponent<DynamicCharacterAvatar>();
+            avatar.CharacterBegun.AddListener(CharacterBegun);
+            context = UMAContext.FindInstance();
+        }
+
+        void CharacterBegun(UMAData umaData)
+        {
+            //If mesh is not dirty then we haven't changed slots.
+            if (!umaData.isMeshDirty)
+                return;
+
+            SlotData[] slots = umaData.umaRecipe.slotDataList;
+
+            foreach(RendererElement element in RendererElements)
+            {
+                wardrobeSlotAssets.Clear();
+
+                //First, lets collect a list of the slotDataAssets that are present in the wardrobe recipes of the wardrobe slots we've specified
+                foreach (string wardrobeSlot in element.wardrobeSlots)
+                {
+                    UMATextRecipe recipe = avatar.GetWardrobeItem(wardrobeSlot);
+                    if (recipe != null)
+                    {
+                        recipe.Load(umaRecipe, context);
+
+                        if (umaRecipe.slotDataList != null)
+                        {
+                            for (int i = 0; i < umaRecipe.slotDataList.Length; i++)
+                            {
+                                SlotData slotData = umaRecipe.slotDataList[i];
+                                if(slotData != null && slotData.asset != null)
+                                    wardrobeSlotAssets.Add(slotData.asset);
+                            }
+                        }
+                    }
+                }
+
+                //Next, check each slot for if they are in the list of specified slots or exist in one of the wardrobe recipes of the wardrobe slot we specified.
+                foreach (SlotData slot in slots)
+                {
+                    if (element.slotAssets.Contains(slot.asset) || wardrobeSlotAssets.Contains(slot.asset))
+                    {
+                        slot.rendererAsset = element.rendererAsset;
+                    }
+                }
+            }
+            wardrobeSlotAssets.Clear();
+        }
+    }
+}
---
+++ UMAProject/Assets/UMA/Core/Extensions/DynamicCharacterSystem/Scripts/Editor/DCARendererManagerEditor.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UMA.CharacterSystem;
+
+namespace UMA.Editors
+{
+    [CustomEditor(typeof(DCARendererManager))]
+    public class DCARendererManagerEditor : Editor
+    {
+        SerializedProperty RendererElements;
+
+        DynamicCharacterAvatar avatar;
+        UMAData.UMARecipe umaRecipe = new UMAData.UMARecipe();
+        List<string> wardrobeOptions = new List<string>();
+        List<SlotDataAsset> slotOptions = new List<SlotDataAsset>();
+        UMAContext context;
+
+        void OnEnable()
+        {
+            RendererElements = serializedObject.FindProperty("RendererElements");
+            context = UMAContext.FindInstance();
+
+            DCARendererManager manager = target as DCARendererManager;
+            avatar = manager.GetComponent<DynamicCharacterAvatar>();
+
+            wardrobeOptions.Clear();
+            slotOptions.Clear();
+
+            if (avatar != null && avatar.activeRace != null && avatar.activeRace.data != null)
+            {
+                wardrobeOptions.AddRange(avatar.activeRace.data.wardrobeSlots);
+                wardrobeOptions.Insert(0, "Add Wardrobe Slot");
+
+                avatar.activeRace.data.baseRaceRecipe.Load(umaRecipe, context);
+                for(int i = 0; i < umaRecipe.slotDataList.Length; i++)
+                {
+                    if (umaRecipe.slotDataList[i] != null && umaRecipe.slotDataList[i].asset != null)
+                    {
+                        slotOptions.Add(umaRecipe.slotDataList[i].asset);
+                    }
+                }                
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            GUILayout.Space(10);
+            if(GUILayout.Button("Add New Renderer Element"))
+            {
+                RendererElements.arraySize++;
+            }
+            GUILayout.Space(20);
+
+            for(int i = RendererElements.arraySize - 1; i >= 0; i--)
+            {
+                EditorGUILayout.BeginVertical("HelpBox");
+                SerializedProperty wardrobeSlots = RendererElements.GetArrayElementAtIndex(i).FindPropertyRelative("wardrobeSlots");
+                SerializedProperty slotAssets = RendererElements.GetArrayElementAtIndex(i).FindPropertyRelative("slotAssets");
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Renderer Element " + i.ToString());
+                if(GUILayout.Button("Remove"))
+                {
+                    RendererElements.DeleteArrayElementAtIndex(i);
+                    continue;
+                }
+                EditorGUILayout.EndHorizontal();
+                GUILayout.Space(10);
+
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(RendererElements.GetArrayElementAtIndex(i).FindPropertyRelative("rendererAsset"));
+                GUILayout.Space(10);
+
+                EditorGUILayout.BeginHorizontal();
+                int newSlot = EditorGUILayout.Popup(0, SlotOptionsToArray(slotOptions));
+                if(newSlot > 0)
+                {
+                    slotAssets.arraySize++;
+                    slotAssets.GetArrayElementAtIndex(slotAssets.arraySize - 1).objectReferenceValue = slotOptions[newSlot-1];
+                }
+
+                int newWardrobe = EditorGUILayout.Popup(0, wardrobeOptions.ToArray());
+                if(newWardrobe > 0)
+                {
+                    if (!ArrayContains(wardrobeSlots, wardrobeOptions[newWardrobe]))
+                    {
+                        wardrobeSlots.arraySize++;
+                        wardrobeSlots.GetArrayElementAtIndex(wardrobeSlots.arraySize-1).stringValue = wardrobeOptions[newWardrobe];
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+                GUILayout.Space(10);
+
+                EditorGUILayout.PropertyField(slotAssets, true);
+                EditorGUILayout.PropertyField(wardrobeSlots, true);
+                EditorGUI.indentLevel--;
+                EditorGUILayout.EndVertical();
+                GUILayout.Space(10);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private bool ArrayContains(SerializedProperty array, string item)
+        {
+            for(int i = 0; i < array.arraySize; i++)
+            {
+                if (array.GetArrayElementAtIndex(i).stringValue == item)
+                    return true;
+            }
+            return false;
+        }
+
+        private string[] SlotOptionsToArray(List<SlotDataAsset> assets)
+        {
+            string[] options = new string[assets.Count + 1];
+            options[0] = "Add Base Slot";
+
+            for (int i = 0; i < assets.Count; i++)
+            {
+                options[i+1] = assets[i].slotName;
+            }
+            return options;
+        }
+    }
+}
