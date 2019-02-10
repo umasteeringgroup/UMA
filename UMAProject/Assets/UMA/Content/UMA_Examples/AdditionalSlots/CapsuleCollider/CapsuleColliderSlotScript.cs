b8ff2400d7b952981597517e4e5bd4bc758a072a
David O'Sullivan <mail@david-osullivan.co.uk>
2019-02-10 17:59:18 +0000

    Made Capsule collider slot script, assign a box collider to non-Humanoid characters like 'SkyCar'

--- UMAProject/Assets/UMA/Content/UMA_Examples/AdditionalSlots/CapsuleCollider/CapsuleColliderSlotScript.cs
+++ UMAProject/Assets/UMA/Content/UMA_Examples/AdditionalSlots/CapsuleCollider/CapsuleColliderSlotScript.cs
@@ -23,9 +23,31 @@ namespace UMA
 			{
 				capsule = umaData.gameObject.AddComponent<CapsuleCollider>();
 			}
-			capsule.radius = umaData.characterRadius;
-			capsule.height = umaData.characterHeight;
-			capsule.center = new Vector3(0, capsule.height / 2, 0);
+			var box = umaData.gameObject.GetComponent<BoxCollider>();
+			if (box == null)
+				box = umaData.gameObject.AddComponent<BoxCollider>();
+
+			if(umaData.umaRecipe.raceData.umaTarget == RaceData.UMATarget.Humanoid)
+			{
+				box.enabled = false;
+				capsule.enabled = true;
+				capsule.radius = umaData.characterRadius;
+				capsule.height = umaData.characterHeight;
+				capsule.center = new Vector3(0, capsule.height / 2, 0);
+			}
+			else
+			{
+				capsule.enabled = false;
+				box.enabled = true;
+				//with skycar this capsule collider makes no sense so we need the bounds to figure out what the size of the box collider should be
+				//we will assume that renderer 0 is the base renderer
+				var umaRenderer = umaData.GetRenderer(0);
+				if (umaRenderer != null)
+				{
+					box.size = umaRenderer.bounds.size;
+					box.center = umaRenderer.bounds.center;
+				}
+			}
 		}
 	}
 }
