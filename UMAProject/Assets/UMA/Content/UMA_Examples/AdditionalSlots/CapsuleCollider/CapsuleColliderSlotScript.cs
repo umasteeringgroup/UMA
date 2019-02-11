a3f9d6c15abd04641d2db5f7ecb62c41f0572808
kenamis <kenan.chabuk@gmail.com>
2019-02-11 09:11:56 -0800

    tweak to add only capsule or box collider and remove other depending on humanoid or not

--- UMAProject/Assets/UMA/Content/UMA_Examples/AdditionalSlots/CapsuleCollider/CapsuleColliderSlotScript.cs
+++ UMAProject/Assets/UMA/Content/UMA_Examples/AdditionalSlots/CapsuleCollider/CapsuleColliderSlotScript.cs
@@ -18,27 +18,35 @@ namespace UMA
 			rigid.constraints = RigidbodyConstraints.FreezeRotation;
 			rigid.mass = umaData.characterMass;
 
-			var capsule = umaData.gameObject.GetComponent<CapsuleCollider>();
-			if (capsule == null)
-			{
-				capsule = umaData.gameObject.AddComponent<CapsuleCollider>();
-			}
-			var box = umaData.gameObject.GetComponent<BoxCollider>();
-			if (box == null)
-				box = umaData.gameObject.AddComponent<BoxCollider>();
+			CapsuleCollider capsule = umaData.gameObject.GetComponent<CapsuleCollider>();
+			BoxCollider box = umaData.gameObject.GetComponent<BoxCollider>();
 
 			if(umaData.umaRecipe.raceData.umaTarget == RaceData.UMATarget.Humanoid)
 			{
-				box.enabled = false;
-				capsule.enabled = true;
+				if (capsule == null)
+				{
+					capsule = umaData.gameObject.AddComponent<CapsuleCollider>();
+				}
+				if( box != null )
+				{
+					Destroy(box);
+				}
+
 				capsule.radius = umaData.characterRadius;
 				capsule.height = umaData.characterHeight;
 				capsule.center = new Vector3(0, capsule.height / 2, 0);
 			}
 			else
 			{
-				capsule.enabled = false;
-				box.enabled = true;
+				if (box == null)
+				{
+					box = umaData.gameObject.AddComponent<BoxCollider>();
+				}
+				if(capsule != null)
+				{
+					Destroy(capsule);
+				}
+
 				//with skycar this capsule collider makes no sense so we need the bounds to figure out what the size of the box collider should be
 				//we will assume that renderer 0 is the base renderer
 				var umaRenderer = umaData.GetRenderer(0);
