e62617f3cade3c48d643d86c658d34b36a780228
Jaimi <jaimi@aztica.com>
2020-02-07 21:16:05 -0600

    Added automated references on build

---
+++ UMAProject/Assets/UMA/Core/Editor/Scripts/BuildPreprocessor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UMA;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildPreprocessor : IPreprocessBuildWithReport
+{
+    public int callbackOrder  { get { return 0; } }
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        UMAAssetIndexer.Instance.AddReferences();
+    }
+}
