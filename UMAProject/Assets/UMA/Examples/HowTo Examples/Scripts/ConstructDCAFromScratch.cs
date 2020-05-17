e8722f7dfc06acdbe947fe904f850c5bca54a71f
Jaimi <jaimi@aztica.com>
2020-05-17 13:45:35 -0500

    Option to load from avatar def on component

--- UMAProject/Assets/UMA/Examples/HowTo Examples/Scripts/ConstructDCAFromScratch.cs
+++ UMAProject/Assets/UMA/Examples/HowTo Examples/Scripts/ConstructDCAFromScratch.cs
@@ -13,32 +13,43 @@ public class ConstructDCAFromScratch : MonoBehaviour
     public RuntimeAnimatorController raceController;
     public List<UMAWardrobeRecipe> wardrobeItems;
     public Color hairColor = Color.red;
+    public bool LoadFromAvatarDef;
+    [TextArea(3,12)]
+    public string AvatarDef;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject go = new GameObject();
         var DCA = go.AddComponent<DynamicCharacterAvatar>();
-
-        // Set the race
-        DCA.RacePreset = raceName;
-        DCA.raceAnimationControllers.defaultAnimationController = raceController;
-
-        // Set any predefined DNA.
-        DCA.predefinedDNA = new UMA.UMAPredefinedDNA();
-        DCA.predefinedDNA.AddDNA("headSize", 0.9f);
-
-        // Setup any wardrobe items you want to preload.
-        foreach(UMAWardrobeRecipe recipe in wardrobeItems)
+        if (LoadFromAvatarDef)
         {
-            DCA.preloadWardrobeRecipes.recipes.Add(new DynamicCharacterAvatar.WardrobeRecipeListItem(recipe));
+            DCA.raceAnimationControllers.defaultAnimationController = raceController;
+            DCA.LoadAvatarDefinition(AvatarDef);
         }
+        else
+        {
 
-        // Setup any predefined colors
-        // This version of "SetColor" only sets the albedo on the first texture 
-        // channel.
-        // If you need full control over color channels, use DCA.SetRawColor("Hair",overlayColorData);
-        DCA.SetColor("Hair", hairColor);
+            // Set the race
+            DCA.RacePreset = raceName;
+            DCA.raceAnimationControllers.defaultAnimationController = raceController;
+
+            // Set any predefined DNA.
+            DCA.predefinedDNA = new UMA.UMAPredefinedDNA();
+            DCA.predefinedDNA.AddDNA("headSize", 0.9f);
+
+            // Setup any wardrobe items you want to preload.
+            foreach (UMAWardrobeRecipe recipe in wardrobeItems)
+            {
+                DCA.preloadWardrobeRecipes.recipes.Add(new DynamicCharacterAvatar.WardrobeRecipeListItem(recipe));
+            }
+
+            // Setup any predefined colors
+            // This version of "SetColor" only sets the albedo on the first texture 
+            // channel.
+            // If you need full control over color channels, use DCA.SetRawColor("Hair",overlayColorData);
+            DCA.SetColor("Hair", hairColor);
+        }
 
         // Set any predefined wardrobe items.
         go.transform.position = new Vector3(0f, 0.5f, 0f);
